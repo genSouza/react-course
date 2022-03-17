f7c7afbcadb1e3e5926ea2bd088531d0918d8843
Genilton Souza <genilton.souza26@gmail.com>
2022-03-17 14:35:56 -0400

    Lesson 33
    Building a First Custom Component

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItemComponent from "./components/ExpenseItem.component";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is also visible</p>
+      <ExpenseItemComponent></ExpenseItemComponent>
+    </div>
+  );
+}
+
+export default App;
