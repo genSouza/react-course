41af0349dda1a346a6cf9cb507b67f044ae0ad08
Genilton Souza <genilton.souza26@gmail.com>
2022-03-18 13:40:31 -0400

    Lesson 35
    Adding Basic CSS Styling

--- react-complete-guide/src/components/ExpenseItem.component.js
+++ react-complete-guide/src/components/ExpenseItem.component.js
@@ -1,5 +1,14 @@
+import "./ExpenseItem.css";
 function ExpenseItemComponent() {
-  return <h2> Expense Item</h2>;
+  return (
+    <div className="expense-item">
+      <div>March 17th 2022</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItemComponent;
