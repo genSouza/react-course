543669bc7c1293c2a48ec354f7a4b7a7f5fb06a3
Genilton Souza <genilton.souza26@gmail.com>
2022-03-18 14:37:44 -0400

    Lesson 38
    Adding "normal" JavaScript Logic to Components

--- react-complete-guide/src/components/ExpenseItem.component.js
+++ react-complete-guide/src/components/ExpenseItem.component.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItemComponent(props) {
+  const month = props.date.toLocaleString("pt-BR", { month: "long" });
+  const day = props.date.toLocaleString("pt-BR", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.date.toDateString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
